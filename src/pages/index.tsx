4b0fe2754e15201ccb00a71a0a66e5eda9c02a91
Frank Laércio <franklaercio@gmail.com>
2021-10-03 19:03:58 -0300

    Adding loading images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import { Error } from '../components/Error';
 export default function Home(): JSX.Element {
   const fetchImages = async ({ pageParam = null }) => {
     const response = await api.get('/api/images', {
-      params: { pageParam: null },
+      params: { after: pageParam },
     });
 
     return response.data;
@@ -42,7 +42,12 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button mt={8} onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
